068c6d1ebf061f9f6e6c74336aeaa5cfed5fb1f8
Alanswe <92720019+Alanswe@users.noreply.github.com>
2022-04-25 13:01:11 +0200

    .

--- admin.py
+++ admin.py
@@ -1,1 +1,15 @@
-# hola
+from bottle import static_file,route,run
+from settings import STATIC_FILES,BD
+from sql import Sql
+
+
+@route('/static/<filename:path>')
+def server_static(filename):
+    archivo = static_file(filename, root=STATIC_FILES)
+    return archivo
+
+@route('/')
+def home():
+    return {'Hola Mundo'}
+
+run(host='localhost', port=8002,debug=True,reloader=True)
