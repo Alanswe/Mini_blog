4d5c5845d28dc5820f25c0134fb24d77ba4ce660
Alanswe <92720019+Alanswe@users.noreply.github.com>
2022-04-28 11:16:15 +0200

    28/04/22

--- admin.py
+++ admin.py
@@ -1,42 +1,73 @@
-from bottle import static_file,route,run,jinja2_view,TEMPLATE_PATH,TEMPLATES,request
-from settings import STATIC_FILES,BD
+from bottle import static_file,route,run,jinja2_view,request,TEMPLATE_PATH,redirect
+from settings import STATIC_FILES,TEMPLATES,BD
 from sql import Sql
+from clase_post import Posts
 
 TEMPLATE_PATH.append(TEMPLATES)
 
 def modifica_fecha(lista_tuplas):
     salida = []
     tmp_tupla = ()
-    pass
+    for t in lista_tuplas:
+        cadena_fecha = ''
+        partes = t[1].split('-')
+        cadena_fecha = partes[2] + ' de ' + partes[1] + ' de ' + partes[0]
+        tmp_tupla = (t[0], cadena_fecha, t[2],t[3],t[4])
+        salida.append(tmp_tupla)
+    return salida
+
+
 
 @route('/static/<filename:path>')
 def server_static(filename):
     archivo = static_file(filename, root=STATIC_FILES)
     return archivo
 
+
 @route('/')
 @jinja2_view('admin_index.html')
 def home():
-    basedatos = Sql(BD)
-    resp = basedatos.select('select p.id,p.fecha,p.autor,p.titulo,p.cuerpo from posts p')
-    return {'posts':resp}
+    bdatos = Sql(BD)
+    resp = bdatos.select('SELECT  p.id, p.fecha, p.autor ,p.titulo, p.cuerpo  from posts p')
+    #resp = modifica_fecha(resp)
+    return {'posts' : resp}
 
 @route('/editar')
 @route('/editar/<id:int>')
 @jinja2_view('admin_formu.html')
 def mi_form(id=None):
-    basedatos = Sql(BD)
+    bdatos = Sql(BD)
     if id:
-        resp = basedatos.select(f'select p.id,p.fecha,p.autor,p.titulo,p.cuerpo from posts p where p.id={id}')
-    return {'posts':resp}
+        resp = bdatos.select(f'SELECT  p.id, p.fecha, p.autor ,p.titulo, p.cuerpo  from posts p where id = {id}')
+    
+    #resp = modifica_fecha(resp)
+    return {'post' : resp[0]}
 
-@route('/guardar',method='POST')
+
+@route('/guardar', method='POST')
 def guardar():
-    id = request.POST.id
+    if request.POST.id:
+        id = request.POST.id
+    else:
+        id = None
+
     fecha = request.POST.fecha
     autor = request.POST.autor
     titulo = request.POST.titulo
     cuerpo = request.POST.cuerpo
-    pass
 
-run(host='localhost', port=8002,debug=True,reloader=True)
+    p = Posts(id,fecha, autor, titulo, cuerpo )
+
+    bdatos = Sql(BD)
+    if request.POST.id:
+        resp = bdatos.update(p)
+    else:
+        resp = bdatos.insert(p)
+
+    redirect('/')
+
+
+
+
+run(host='localhost', port=8000,debug=True,reloader=True)
+
